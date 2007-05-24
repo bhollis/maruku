f1d0e0446ebb66328ca8c0b3135169dbabcbb199
Andrea Censi <andrea@cds.caltech.edu>
2007-05-24 13:47:10 +0000

    Plugin for movable type
    git-svn-id: svn://rubyforge.org/var/svn/maruku/trunk@143 7e2f57f0-5a75-4fd6-9797-5817c466b243

---
+++ tools/mt-maruku-plugin.pl
@@ -0,0 +1,46 @@
+package MT::Plugins::Maruku;
+use vars qw( $MARUKU );
+
+$MARUKU = "/usr/bin/maruku.sh";
+
+
+
+use MT;
+
+MT->add_text_filter('Maruku' => {
+  label => 'Maruku',
+  docs => 'http://maruku.rubyforge.org',
+  on_format => \&maruku
+});
+
+sub maruku {
+	use File::Temp qw/ tempfile tempdir /;
+	my $text=shift;
+	my $ctx=shift;
+
+	($fh, $filename) = tempfile();
+	print $fh  $text;
+	close $fh ;
+
+	$fileout = $filename.".out"; 
+
+	@args = (
+		$MARUKU, 
+		"--html-frag", 
+		"-o", $fileout,
+		$filename);
+    system(@args) == 0
+	 or die "system @args failed: $?";
+	
+	# read entire file
+	$result  = "";
+	open SLURP, $fileout or die "can't open  $fileout: $!";
+	while ($data = <SLURP>) {
+		$result = $result . $data;
+	}
+	close SLURP or die "cannot close  $fileout: $!";
+
+#	 "<h2>maruku</h2>" . $filename . "<br>-----<br>". $text .  "<br>-----<br>" . $result . "<br>-----<br>" ;
+
+	$result;
+}
