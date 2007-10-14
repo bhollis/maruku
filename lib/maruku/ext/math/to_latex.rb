41114cb687b6de0a1fb419ce041cee053d5f0f25
Andrea Censi <andrea@cds.caltech.edu>
2007-10-14 00:47:13 +0000

    git-svn-id: svn://rubyforge.org/var/svn/maruku/trunk@160 7e2f57f0-5a75-4fd6-9797-5817c466b243

--- lib/maruku/ext/math/to_latex.rb
+++ lib/maruku/ext/math/to_latex.rb
@@ -3,17 +3,15 @@ require 'maruku/ext/math/latex_fix'
 module MaRuKu; module Out; module Latex
 
 	def to_latex_inline_math
-		"$#{self.math.strip}$"
+		"$#{self.math.strip}$".fix_latex
 	end
 
 	def to_latex_equation
 		if self.label
 			l =  "\\label{#{self.label}}"
-			# "\\begin{equation}\n#{self.math.strip}\n#{l}\\end{equation}\n"
 			"\\begin{equation}\n#{self.math.strip}\n#{l}\\end{equation}\n".fix_latex
 		else
-			# "\\begin{displaymath}\n#{self.math.strip}\n\\end{displaymath}\n"
-				"\\begin{displaymath}\n#{self.math.strip}\n\\end{displaymath}\n".fix_latex
+			"\\begin{displaymath}\n#{self.math.strip}\n\\end{displaymath}\n".fix_latex
 		end
 	end
 	
