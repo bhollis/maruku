c58d3e59a4506c14c032939eae40782fbbe03a6f
Nathan Weizenbaum <nex342@gmail.com>
2010-03-04 13:35:28 -0800

    Move the monkeypatched String#fix_latex to a method in the Latex module.

--- lib/maruku/ext/math/to_latex.rb
+++ lib/maruku/ext/math/to_latex.rb
@@ -3,15 +3,15 @@ require 'maruku/ext/math/latex_fix'
 module MaRuKu; module Out; module Latex
 
 	def to_latex_inline_math
-		"$#{self.math.strip}$".fix_latex
+		fix_latex("$#{self.math.strip}$")
 	end
 
 	def to_latex_equation
 		if self.label
 			l =  "\\label{#{self.label}}"
-			"\\begin{equation}\n#{self.math.strip}\n#{l}\\end{equation}\n".fix_latex
+			fix_latex("\\begin{equation}\n#{self.math.strip}\n#{l}\\end{equation}\n")
 		else
-			"\\begin{displaymath}\n#{self.math.strip}\n\\end{displaymath}\n".fix_latex
+			fix_latex("\\begin{displaymath}\n#{self.math.strip}\n\\end{displaymath}\n")
 		end
 	end
 	
@@ -23,4 +23,12 @@ module MaRuKu; module Out; module Latex
        "\\ref{#{self.refid}}"
 	end
 
+	private
+
+	def fix_latex(str)
+		return str unless self.get_setting(:html_math_engine) == 'itex2mml'
+		s = str.gsub("\\mathop{", "\\operatorname{")
+		s.gsub!(/\\begin\{svg\}.*?\\end\{svg\}/m, " ")
+		s.gsub("\\space{", "\\itexspace{")
+	end
 end end end
