cbdfdecc5a64d243bb8f782841985f751acc24c8
Ben Hollis <ben@benhollis.net>
2013-01-19 13:25:00 -0800

    Clean up helpers.rb and structures_inspect.rb. Move all inspect functionality into structures_inspect.rb. Make inspect output significantly more compact.

--- lib/maruku/structures_inspect.rb
+++ lib/maruku/structures_inspect.rb
@@ -19,33 +19,68 @@
 
 module MaRuKu
   class MDElement
-    def inspect(compact=true)
+    INSPECT2_FORMS = {
+      :paragraph          => ["par",      :children],
+      :footnote_reference => ["foot_ref", :footnote_id],
+      :entity             => ["entity",   :entity_name],
+      :email_address      => ["email",    :email],
+      :inline_code        => ["code",     :raw_code],
+      :raw_html           => ["html",     :raw_html],
+      :emphasis           => ["em",       :children],
+      :strong             => ["strong",   :children],
+      :immediate_link     => ["url",      :url],
+      :image              => ["image",    :children, :ref_id],
+      :im_image           => ["im_image", :children, :url, :title],
+      :link               => ["link",     :children, :ref_id],
+      :im_link            => ["im_link",  :children, :url, :title],
+      :ref_definition     => ["ref_def",  :ref_id, :url, :title],
+      :ial                => ["ial",      :ial],
+      :li                 => ["li",       :children, :want_my_paragraph]
+    }
+
+    def inspect(compact = true)
       if compact
         i2 = inspect2
         return i2 if i2
       end
 
-      # Make sure the attributes are lexically ordered
-      meta_ordered = "{" + @meta_priv.keys.
-        map {|x| x.to_s}.sort.map {|x| x.to_sym}.
-        map {|k| k.inspect + "=>" + @meta_priv[k].inspect}.
-        join(',') + "}"
+      params = [
+                self.node_type.inspect,
+                children_inspect(compact)
+               ]
 
-      "md_el(%s,%s,%s,%s)" % [
-        self.node_type.inspect,
-        children_inspect(compact),
-        meta_ordered,
-        self.al.inspect
-      ]
+      params << @meta_priv.inspect unless @meta_priv.empty?
+      params << self.al.inspect unless self.al.empty?
+
+      "md_el(#{params.join(', ')})"
+    end
+
+    # Outputs the abbreviated form of an element
+    # (this should be `eval`-able to get a copy of the original element).
+    def inspect2
+      name, *params = INSPECT2_FORMS[@node_type]
+      return nil unless name
+
+      params = params.map do |p|
+        next children_inspect if p == :children
+        send(p).inspect
+      end
+      params << @al.inspect if @al && !@al.empty?
+
+      "md_#{name}(#{params.join(', ')})"
     end
 
     def children_inspect(compact=true)
       kids = @children.map {|x| x.is_a?(MDElement) ? x.inspect(compact) : x.inspect}
+      return kids.first if kids.size == 1
+
       comma = kids.join(", ")
 
-      return "[#{comma}]" if comma.size < 70
-      "[\n\t#{kids.join(",\n\t")}\n]"
+      if comma.size < 70
+        "[#{comma}]"
+      else
+        "[\n\t#{kids.join(",\n\t")}\n]"
+      end
     end
-
   end
 end
