be898f7de562f4cee19f9653bae7870fe3b57d26
Ben Hollis <ben@benhollis.net>
2013-02-18 00:15:37 -0800

    Remove double ticks test, smartypants really should turn them into left double quotes.

--- spec/span_spec.rb
+++ spec/span_spec.rb
@@ -28,7 +28,6 @@ EXPECTATIONS = Maruku.new.instance_eval do
     ["`` \\` ``" ,    [md_code('\\`')],   ],
     ["``a``" ,   [md_code('a')],    ],
     ["`` a ``" ,   [md_code('a')],    ],
-    ["``a", ["``a"], 'Unmatched double ticks are left alone', true ],
 
     # Newlines
     ["a  \n", ['a',md_el(:linebreak)], 'Two spaces give br.'],
