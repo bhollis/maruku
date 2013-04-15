4c9d923a5b35d39b131c3bd9a4ed1d252c41fd12
Jacques Distler <distler@golem.ph.utexas.edu>
2013-04-15 00:28:55 -0500

    Restore defaults

--- lib/maruku/defaults.rb
+++ lib/maruku/defaults.rb
@@ -13,7 +13,7 @@ module MaRuKu
     :filter_html => false,
 
     :html_math_output_mathml => true, # also set :html_math_engine
-    :html_math_engine => 'itex2mml', # none, ritex, itex2mml
+    :html_math_engine => 'none', # none, ritex, itex2mml
 
     :html_math_output_png => false,
     :html_png_engine => 'none',
@@ -21,7 +21,7 @@ module MaRuKu
     :html_png_url => 'pngs/',
     :html_png_resolution => 200,
 
-    :html_use_syntax => true,
+    :html_use_syntax => false,
 
     :latex_use_listings => false,
     :latex_cjk => false,
