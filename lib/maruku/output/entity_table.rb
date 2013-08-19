0c6dccdf2aaca1831bacd5bf710abeb38281b5ac
Ben Hollis <ben@benhollis.net>
2013-08-18 18:06:18 -0700

    Use REXML to load the entities table (once, at startup) rather than Nokogiri.

--- lib/maruku/output/entity_table.rb
+++ lib/maruku/output/entity_table.rb
@@ -1,4 +1,4 @@
-require 'nokogiri'
+require 'rexml/document'
 require 'singleton'
 
 module MaRuKu::Out
@@ -11,13 +11,13 @@ module MaRuKu::Out
     def initialize
       @entity_table = {}
 
-      xml = File.read(File.join(File.dirname(__FILE__), '..', '..', '..', 'data', 'entities.xml'))
-      doc = Nokogiri::XML::Document.parse(xml)
-      doc.xpath("//char").each do |c|
-        num = c['num'].to_i
-        name = c['name']
-        convert = c['convertTo']
-        package = c['package']
+      xml = File.new(File.join(File.dirname(__FILE__), '..', '..', '..', 'data', 'entities.xml'))
+      doc = REXML::Document.new(xml)
+      doc.elements.each("//char") do |c|
+        num = c.attributes['num'].to_i
+        name = c.attributes['name']
+        convert = c.attributes['convertTo']
+        package = c.attributes['package']
 
         e = Entity.new(num, name, convert, package)
         @entity_table[name] = e
